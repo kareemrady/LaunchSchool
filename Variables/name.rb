21fe0884dcbe62e787f5caeaab3a306431d93c2d
Kareem Rady <kareem_rady@yahoo.com>
2016-07-28 18:52:11 -0600

    using times to print 10 times and getting first name and last name and concatenating the output

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,4 +1,7 @@
-puts "Please input your name below: "
-name = gets.chomp
+puts "Please input your first name below: "
+first_name = gets.chomp
+puts "Please input your second name below: "
+last_name = gets.chomp
 
-puts "Your name is #{name}"
+
+10.times {puts "Your name is #{first_name} #{last_name}"}
