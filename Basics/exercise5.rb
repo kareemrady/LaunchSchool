782e89d0feb1e364f9896e5a33fd150715fcedfb
Kareem Rady <kareem_rady@yahoo.com>
2016-07-28 18:33:11 -0600

    Exercise5 adding recusrive factorial method

--- Basics/exercise5.rb
+++ Basics/exercise5.rb
@@ -1,7 +1,9 @@
 #Factroials
 # 5! = 5 * 4 * 3 * 2 * 1
 
+
 def factrorial(num)
+  return 1 if num == 1 || num == 0
   product = num
   while num > 1
     product *= num - 1
@@ -14,3 +16,10 @@ puts "Factorial of 5 is : #{factrorial(5)}"
 puts "Factorial of 6 is : #{factrorial(6)}"
 puts "Factorial of 7 is : #{factrorial(7)}"
 puts "Factorial of 8 is : #{factrorial(8)}"
+
+
+def factroial_recursive(num)
+  return 1 if num == 1 ||  num == 0
+  num * factroial_recursive(num-1)
+end
+puts "Recursive Factroial of 5 is : #{factroial_recursive(5)}"
