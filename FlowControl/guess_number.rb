120865c9399792192cb77d45f517254a00882137
Kareem Rady <kareem_rady@yahoo.com>
2016-07-28 19:33:37 -0600

    Case num function

--- FlowControl/guess_number.rb
+++ FlowControl/guess_number.rb
@@ -12,5 +12,20 @@ else
   puts "Number greater than 100"
 end
     
+
+def case__guessnum(num)
+  case
+  when num < 0
+    puts "Out of range number cannot input a number smaller than zero"
+  when num <= 50
+    puts "You guessed a number between 0 and 50"
+  when num <= 100
+    puts "You guessed a number between 50 and 100"
+  else
+    puts "Number greater than 100"
+  end
+end
     
+puts "Case num function"
+puts case__guessnum(60)
     
