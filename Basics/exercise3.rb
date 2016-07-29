bd36606f9e82573fc577241f5b5a26e5074e2102
Kareem Rady <kareem_rady@yahoo.com>
2016-07-28 18:04:56 -0600

    Exercise 3 Movie Hash

---
+++ Basics/exercise3.rb
@@ -0,0 +1,5 @@
+movies_hash = {movie1: 1975, movie2: 2004, movie3: 2013, movie4: 2001, movie5: 1981}
+
+movies_hash.each {|k, v| puts "#{k} movie released in #{v} year"}
+
+#puts movies_hash[:movie1] .....
