165d4993f1ee1b43d4f634266e4d94f9edc3f3b2
Kareem Rady <kareem_rady@yahoo.com>
2016-07-28 22:50:02 -0600

    Using While loop for Exercise 2

--- LoopIterators/exercise2.rb
+++ LoopIterators/exercise2.rb
@@ -1,7 +1,6 @@
-loop do 
+input = ""
+while input != "STOP" do 
   puts "Please Enter an input below or type STOP to stop :"
   input = gets.chomp
   puts "you typed #{input}"
-  break if input == "STOP"
-
 end
