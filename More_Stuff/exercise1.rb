64b3eefa5587f17015e91a0e2fad4b97aa40fa55
Kareem Rady <kareem_rady@yahoo.com>
2016-07-30 15:19:09 -0600

    Iterate over an array using each

--- More_Stuff/exercise1.rb
+++ More_Stuff/exercise1.rb
@@ -3,3 +3,6 @@ words = ["laboratory", "experiment", "Pans Labyrinth", "elaborate", "polar bear"
 lab_matches = words.select {|word| /(lab)/.match(word)}
 
 lab_matches.each{|match| puts match}
+
+
+
