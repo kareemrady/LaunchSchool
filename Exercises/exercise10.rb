ca394d0914b3de5b7eb927f392c2942dbd65e8f5
Kareem Rady <kareem_rady@yahoo.com>
2016-07-30 15:37:27 -0600

    Array of hashes and hashes with array values examples

---
+++ Exercises/exercise10.rb
@@ -0,0 +1,26 @@
+# hash values are arrays Example
+
+
+h1 = {a: ["apple", "aside", "adam", "alvin"], b: ["bob", "bicycle", "broom", "block"]}
+
+puts h1.values[0].class
+
+# Array of hashes
+
+arr_hashes = [ {
+
+  a: 1,
+  b: 2,
+  c: 3
+
+  },
+   {
+    d: 4,
+    e: 5,
+    f: 6
+
+   }
+
+]
+
+puts arr_hashes[0].class
